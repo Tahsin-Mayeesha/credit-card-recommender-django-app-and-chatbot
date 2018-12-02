ff7bb849d8732eef40e0f39866b94eac6c905ff3
Tahsin-Mayeesha <tasmiah.tahsin@northsouth.edu>
2018-12-02 12:33:29 +0600

    Dataset more cleanup, django import-export app added for easy database population

--- card_recommender/recommender/admin.py
+++ card_recommender/recommender/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from import_export.admin import ImportExportModelAdmin
+from .models import Person
+
+@admin.register(Person)
+class PersonAdmin(ImportExportModelAdmin):
+    pass
--- card_recommender/recommender/models.py
+++ card_recommender/recommender/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True)
+    birth_date = models.DateField()
+    location = models.CharField(max_length=100, blank=True)
