59289ae2ba52506bae58833b7ee913100a2cab7c
Tashfiqanotherone <arafat.prantik@gmail.com>
2018-12-15 18:33:17 +0600

    Added machine learning model with the webhook

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, make_response, jsonify
 import json
+from sklearn.externals import joblib
+import numpy as np
+import os
+from recommender import user_features,make_query
 
 # initialize the flask app
 app = Flask(__name__)
+
+def make_recommendations():
+    pass
 
 # default route
 @app.route('/')
@@ -15,7 +22,14 @@ def webhook():
     req = request.get_json(force=True)
     action = req.get("queryResult").get("action")
     parameters = req.get("queryResult").get("parameters")
-    return make_response(jsonify({'fulfillmentText': "I want a card"}))
+    query = make_query(user_features(parameters))
+    full_dir = "knn.pickle"
+    model = joblib.load(os.path.realpath(full_dir))
+    query = np.array(query)
+    print(query)
+    result = model.kneighbors(query.reshape(1,-1))
+    print(result)
+    return make_response(jsonify({'fulfillmentText': str(result)}))
 # run the app
 if __name__ == '__main__':
    app.run()
