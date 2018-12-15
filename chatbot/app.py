0f8cf5e91ef17a2f2919be510e12a4529d06b650
Tashfiqanotherone <arafat.prantik@gmail.com>
2018-12-16 02:24:18 +0600

    Remove bad commits

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,15 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 import json
-from sklearn.externals import joblib
-import numpy as np
-import os
-from recommender import user_features,make_query
 
 # initialize the flask app
 app = Flask(__name__)
-
-def make_recommendations():
-    pass
 
 # default route
 @app.route('/')
@@ -22,14 +15,7 @@ def webhook():
     req = request.get_json(force=True)
     action = req.get("queryResult").get("action")
     parameters = req.get("queryResult").get("parameters")
-    query = make_query(user_features(parameters))
-    full_dir = "knn.pickle"
-    model = joblib.load(os.path.realpath(full_dir))
-    query = np.array(query)
-    print(query)
-    result = model.kneighbors(query.reshape(1,-1))
-    print(result)
-    return make_response(jsonify({'fulfillmentText': str(result)}))
+    return make_response(jsonify({'fulfillmentText': "I want a card"}))
 # run the app
 if __name__ == '__main__':
    app.run()
